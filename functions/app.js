57c211b30bde898de84278fd292106473cb5284a
ToluAdeoye <toluwaloope.adeoye@habaripay.com>
2024-07-25 22:59:14 +0100

    updated app

--- functions/app.js
+++ functions/app.js
@@ -3,16 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
-const placesRouter = require("./routes/places");
-
-app.use(express.json());
+const placesRouter = require("../routes/places");
 
 app.use("/.netlify/functions/app/places", placesRouter);
-
-const PORT = process.env.PORT || 3003;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports.handler = serverless(app);
