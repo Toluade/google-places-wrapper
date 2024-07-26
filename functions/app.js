48494da2f6a29bf24c3a9196e2f679569275dfe4
ToluAdeoye <toluwaloope.adeoye@habaripay.com>
2024-07-26 13:42:07 +0100

    cors handling

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const placesRouter = require("../routes/places");
 
+app.use(cors()); // Allows all origins by default
+app.options("*", cors()); // Preflight requests
 app.use(express.json());
 
 app.use("/api", placesRouter);
